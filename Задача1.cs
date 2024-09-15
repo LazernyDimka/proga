2a08baf6ec1c016211dd6fd7f6823072731f9d6c
LazerDim700 <pankovegor00@gmail.com>
2024-09-15 10:58:26 +0300

    Create Задача1.cs

---
+++ Задача1.cs
@@ -0,0 +1,14 @@
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        int a, b;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(a - a + b);
+        Console.WriteLine(b - b + a);
+    }
+}
+/* «адача:помен€ть значение переменных метсами
