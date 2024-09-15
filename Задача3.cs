9315bec99da2fbbcf370ecef262327078b84ab87
LazerDim700 <pankovegor00@gmail.com>
2024-09-15 11:51:31 +0300

    Create Задача3.cs

---
+++ Задача3.cs
@@ -0,0 +1,17 @@
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        int k, l, m, n, rez;
+        n = Convert.ToInt32(Console.ReadLine());
+        l = 3;
+        m = 3;
+        k = 5;
+        rez = (((2 * m + l) * n) + (((k + l) * 2 + (2 * k + l * n * 2)) / 2 * n)) - l * n;
+
+        Console.WriteLine(rez);
+    }
+}
+/* задача про грядки
