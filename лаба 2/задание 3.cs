268256fac338bf85cbf557e87c096cf9460dd68a
LazerDim700 <90453234+LazernyDimka@users.noreply.github.com>
2024-11-08 17:57:31 +0300

    Add files via upload

---
+++ лаба 2/задание 3.cs
@@ -0,0 +1,23 @@
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int x = 0;
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i < num; i++)
+        {
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (b < a && b < c)
+            {
+                x++;
+            }
+            a = b;
+            b = c;
+        }
+        Console.WriteLine(x);
+    }
+}
+//определить количество элементов, которые являются локальными минимумами//
